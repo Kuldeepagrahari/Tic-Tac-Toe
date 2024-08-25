cfe583722bbb1dd0e1839b025e57f10e6ee202ad
Kuldeep Agrahari <133698085+Kuldeepagrahari@users.noreply.github.com>
2024-08-25 23:52:51 +0530

    some audio effects

--- tic-tak-toe/src/board.jsx
+++ tic-tak-toe/src/board.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react'
 import cross from "./Assets/cross.png"
 import circle from "./Assets/circle.png"
-
+import winSound from "./Assets/win.mp3"
+import gameSound from "./Assets/game.mp3"
+import  chanceSound from "./Assets/chance.mp3"
 let data = ["","","","","","","","","",""]
 const Board = () => {
+    const winAudio = new Audio(winSound);
+    const gameAudio = new Audio(gameSound);
+    const chanceAudio = new Audio(chanceSound);
 
     const [botStyle, setBotStyle] = useState("btn1")
     const [friendStyle, setFriendStyle] = useState("friend")
@@ -11,50 +16,83 @@ const Board = () => {
     const [botVal, setBotVal] = useState("Play with our Bot")
     const [flag, setFlag] = useState("circle")
     const [lock, setLock] = useState(false)
-    const [gameResult, setGameResult] = useState("just click on your best box !")
-
+    const [gameResult, setGameResult] = useState("Click on Best Box!")
+    function winBackground (a,b,c){
+      document.getElementById(`b${a}`).style.boxShadow="2px 2px 20px inset gold"
+      document.getElementById(`b${b}`).style.boxShadow="2px 2px 20px inset gold"
+      document.getElementById(`b${c}`).style.boxShadow="2px 2px 20px inset gold"
+      
+    }
     function checkWin(){
         if(friendVal === "Friend v/s Friend ON!")
        { if(data[0] === data[1] && data[1] === data[2] && data[0] !== ""){
             winEffect(data[0])
+            var a = 0, b = 1, c  = 2
+            winBackground(a, b, c)
+            console.log("sam")
+
         }
         else if(data[0] === data[4] && data[4] === data[8] && data[8] !== ""){
             winEffect(data[0])
+            var a = 0, b = 4, c  = 8
+            winBackground(a, b, c)
+            console.log("sam")
         }
         else if(data[2] === data[4] && data[4] === data[6] && data[6] !== ""){
             winEffect(data[2])
+            var a = 2, b = 4, c  = 6
+            winBackground(a, b, c)
+            console.log("sam")
         }
         else if(data[3] === data[4] && data[4] === data[5] && data[3] !== ""){
             winEffect(data[3])
+            var a = 3, b = 4, c  = 5
+            winBackground(a, b, c)
+            console.log("sam")
         }
         else if(data[6] === data[7] && data[7] === data[8] && data[6] !== ""){
             winEffect(data[6])
+            var a = 6, b = 7, c  = 8
+            winBackground(a, b, c)
+            console.log("sam")
         }
         else if(data[0] === data[3] && data[3] === data[6] && data[0] !== ""){
             winEffect(data[0])
+            var a = 0, b = 3, c  = 6
+            winBackground(a, b, c)
+            console.log("sam")
         }
         else if(data[1] === data[4] && data[4] === data[7] && data[1] !== ""){
             winEffect(data[1])
+            var a = 1, b = 4, c  = 7
+            winBackground(a, b, c)
+            console.log("sam")
         }
         else if(data[2] === data[5] && data[5] === data[8] && data[8] !== ""){
             winEffect(data[2])
+            var a = 2, b = 5, c  = 8
+            winBackground(a, b, c)
+            console.log("sam")
         }}
-       
+        chanceAudio.play()
 
       
        
     }
     function winEffect(winner){
           setLock(true)
+          winAudio.play()
         //   const announcer = document.getElementById("result")
           if (winner === 'x')
             {
-             setGameResult(`Congratulations!  <img src=${cross} alt="circle" / > , you Win!`)
+             setGameResult(`Congratulations <img src=${cross} alt="circle" / > , you Win!`)
             //  announcer.innerHTML=`<img src=${cross}/>`
 
             }
           else if (winner === 'o'){
             setGameResult(`Congratulations!  <img src=${circle} alt="circle" / >  ,  you Win!`)
+
+
             // announcer.innerHTML=`<img src=${circle}/>`
           }
         //   else {
@@ -80,8 +118,8 @@ const Board = () => {
 {       if ( lock === true ){
         return 0;
        }
-
-       if ( flag === "circle" ){
+        if(!data[ind]){
+       if (  flag === "circle" ){
           
            event.target.innerHTML = `<img src = ${circle} />`
            setFlag("cross")
@@ -91,7 +129,7 @@ const Board = () => {
            event.target.innerHTML = `<img src = ${cross} />`
            setFlag("circle")
            data[ind] = 'x'
-       }
+       }}
 
        checkWin()
        
@@ -100,7 +138,7 @@ const Board = () => {
             if ( data[i] === 'o' || data[i] === 'x' )c++
        }
        console.log(c)
-       if ( c === 9 )if(gameResult === "just click on your best box !")setGameResult("Game Draw!, Play Again")
+       if ( c === 9 )if(gameResult === "just click on your best box !")setGameResult("Game Draw! Play Again")
        console.log(gameResult)
     
     }
@@ -115,6 +153,11 @@ const Board = () => {
        }
        setLock(false)
        setGameResult("just click on your best box !")
+    //    console.log(divs[0].style.backgroundColor)
+       for(var i = 0; i<9; i++)
+       divs[i].style.boxShadow=""
+
+    //    console.log(divs[0].style.backgroundColor)
        
     //    console.log(data[0] + data[1] + data[2])
       
@@ -155,25 +198,25 @@ const Board = () => {
                 <div  className="row1">
                     <div onClick={(event)=>{
                     toggle(event,0) 
-                }} className="box"></div><div onClick={(event)=>{
+                }} className="box" id="b0"></div><div onClick={(event)=>{
                     toggle(event,1)
-                }} className="box"></div><div onClick={(event)=>{
+                }} className="box" id="b1"></div><div onClick={(event)=>{
                     toggle(event,2)
-                }} className="box"></div></div>
+                }} className="box" id="b2"></div></div>
                 <div className="row2"><div onClick={(event)=>{
                     toggle(event,3)
-                }} className="box"></div><div onClick={(event)=>{
+                }} className="box" id="b3"></div><div onClick={(event)=>{
                     toggle(event,4)
-                }} className="box"></div><div onClick={(event)=>{
+                }} className="box" id="b4"></div><div onClick={(event)=>{
                     toggle(event,5)
-                }} className="box"></div></div>
+                }} className="box" id="b5"></div></div>
                 <div className="row3"><div onClick={(event)=>{
                     toggle(event,6)
-                }} className="box"></div><div onClick={(event)=>{
+                }} className="box" id="b6"></div><div onClick={(event)=>{
                     toggle(event,7)
-                }} className="box"></div><div onClick={(event)=>{
+                }} className="box" id="b7"></div><div onClick={(event)=>{
                     toggle(event,8)
-                }} className="box"></div></div>
+                }} className="box" id="b8"></div></div>
             </div>
             
         </div>
--- tic-tak-toe/src/nav.jsx
+++ tic-tak-toe/src/nav.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
-
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaSquareGithub } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import {Link} from "react-router-dom"
 const Nav = () => {
   return (
     <div>
       <nav>
         <h1>Tic Tac Toe</h1>
-        
-        <h10 id="brand"><p>by</p> <a href="https://x.com/kuldeep_106">@Kuldeep</a></h10>
+        <div className="social">
+          <Link to="https://github.com/Kuldeepagrahari">< FaSquareGithub style={{backgroundImage: "linear-gradient(45deg, gold,white)", color:"black", width:"25", height:"25", borderRadius:"5px"}}/></Link>
+          <Link to="https://www.linkedin.com/in/kuldeep-agrahari-56b159260"><FaLinkedin style={{backgroundImage: "linear-gradient(45deg, gold,white)", color:"black",width:"25", height:"25", borderRadius:"5px"}}/></Link>
+          <Link to="https://x.com/kuldeep_106"><FaSquareXTwitter style={{backgroundImage: "linear-gradient(45deg, gold,white)", color:"black",width:"25", height:"25", borderRadius:"5px"}}/></Link>
+        </div>
       </nav>
     </div>
   )
